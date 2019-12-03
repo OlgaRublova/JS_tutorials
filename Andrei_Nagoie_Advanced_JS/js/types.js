ad1f8f911fa41721b7b52cb92f819018d0dd5454
olga <olga.i.rubleva@gmail.com>
2019-12-02 19:58:11 -0800

    - practised type coercion

---
+++ Andrei_Nagoie_Advanced_JS/js/types.js
@@ -0,0 +1,41 @@
+//exersice 1
+// const number = 100;
+// const string = "Jay";
+// let obj1 = {
+//     value: "a"
+// };
+// let obj2 = {
+//     value: "b"
+// };
+// let obj3 = obj2;
+//
+//
+// function change(number, string, obj1, obj2) {
+//     number = number * 10;
+//     string = "Pete";
+//     obj1 = obj2;
+//     obj2.value = "c";
+// }
+//
+// change(number, string, obj1, obj2);
+// console.log(number);//100
+// console.log(string); //Jay
+// console.log(obj1.value);//a
+
+//type coercion
+//coercion - converting one type of data  to another
+//false == ""  - true
+// "" == 0 - true
+//
+//false == []  - true
+// "" == [] - true
+// 0 == [] - true
+//
+//false == {}  - false
+// "" == {} - false
+// 0 == {} - false
+//
+// 0 == null - false
+
+
+
